ec3dabd4983952a19d2a719750398780eb984662
Richard Venable <richard@epicfox.com>
2012-03-31 21:11:00 -0500

    Fixed ref search input ajax

--- theme/bfox-tool-content-primary-search.php
+++ theme/bfox-tool-content-primary-search.php
@@ -11,7 +11,7 @@ $toolContext = $bfox->tools->currentContext();
 
 ?>
 		<form method="get" id="bible-form" action="<?php echo esc_url( $bfox->tools->urlForToolName() ); ?>" class="bfox-tool-form">
-			<input type="text" id="bfox-tool-ref-global" class="field bfox-tool-ref" name="ref" placeholder="<?php esc_attr_e( 'Search' ); ?>" value="<?php echo $refContext->ref->get_string(BibleMeta::name_short) ?>" />
+			<input type="text" id="bfox-tool-ref-global" class="field <?php echo $refContext->updaterClass ?>" <?php echo $refContext->dependencySelectorAttribute(); ?> name="ref" placeholder="<?php esc_attr_e( 'Search' ); ?>" value="<?php echo $refContext->ref->get_string(BibleMeta::name_short) ?>" />
 			<select class="bfox-tool-name <?php echo $toolContext->updaterClass ?>" id="bfox-tool-name-main" <?php echo $toolContext->dependencySelectorAttribute(); ?> name="tool">
 				<?php echo $toolContext->selectOptions(); ?>
 			</select>

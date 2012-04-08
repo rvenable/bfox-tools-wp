f8db6f2ae9853d4670c24c4b1a04f906d1d81d33
Richard Venable <richard@epicfox.com>
2012-04-07 19:44:28 -0500

    Removed obsolete javascript methods (most of these moved to the core plugin)

--- bfox_tool.js
+++ bfox_tool.js
@@ -4,70 +4,11 @@ jQuery(document).ready(function () {
 	'use strict';
 
 	/*
-	 * Add BfoxAjax functions
-	 */
-	
-	BfoxAjax.toolValueChanged = function (id, key, value) {
-		var parameters, url, updatingElement, obj;
-
-		obj = { 'id': id };
-		obj[key] = value;
-		parameters = jQuery.param(obj);
-
-		jQuery('.depends-' + id).each(function () {
-			updatingElement = this;
-			url = jQuery(updatingElement).attr('data-url');
-			url = BfoxAjax.appendUrlWithParamString(url, parameters);
-
-			jQuery.get(url, function (response) {
-				jQuery(updatingElement).attr('data-url', response.dataUrl);
-				jQuery(updatingElement).html(response.html);
-			});
-		});
-
-		return false;
-	};
-	
-	BfoxAjax.refValueChanged = function (element) {
-		return BfoxAjax.toolValueChanged(jQuery(element).attr('id'), 'ref', jQuery(element).val());
-	};
-
-	BfoxAjax.toolNameValueChanged = function (element) {
-		return BfoxAjax.toolValueChanged(jQuery(element).attr('id'), 'tool', jQuery(element).val());
-	};
-
-	/*
 	 * Set up DOM elements
 	 */
-	
-	// input.bfox-tool-ref should update Bible references on keyboard enter
-	jQuery('input.bfox-tool-ref').keypress(function (e) {
-		if (e.which == 13) {
-			BfoxAjax.refValueChanged(this);
-			e.preventDefault();
-			return false;
-		}
-	});
-
-	// .bfox-tool-ref-refresh should refresh once after the page loads
-	jQuery('.bfox-tool-ref-refresh').each(function () {
-		BfoxAjax.refValueChanged(this);
-	});
 	
 	// select.bfox-tool-name should update the Bible tool on value change 
 	jQuery('select.bfox-tool-context-updater').live('change', function () {
 		return BfoxAjax.refreshSelectorForKeyValue(jQuery(this).attr('data-selector'), 'tool', jQuery(this).val());
-	});
-	
-	// a.bfox-ref-update links should update the ref value for a specified selector
-	jQuery('a.bfox-ref-update').live('click', function () {
-		var selector, ref;
-		selector = jQuery(this).attr('data-selector');
-		ref = jQuery(this).attr('data-ref');
-		jQuery(selector).val(ref).each(function () {
-			BfoxAjax.refValueChanged(this);
-		});
-		
-		return false;
 	});
 });
